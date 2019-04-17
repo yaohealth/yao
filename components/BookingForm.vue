1e7845ca87d61598605b31fb20e303f757633da2
Patrick Weingaertner <patrick.weingaertner91@gmail.com>
2019-04-17 10:21:36 +0200

    add calendar and booking components

---
+++ components/BookingForm.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-container>
+
+      <v-dialog width="60vw" v-model="show">
+        <v-card>
+          <v-layout  column justify-center fluid>
+            <v-flex>
+              <v-card-title>
+                <h1>Termin buchen</h1>
+              </v-card-title>
+              <v-card-title>
+                <span>{{selectedDate}}</span>
+              </v-card-title>
+            </v-flex>
+            <v-flex>
+              <v-card-text>
+                <v-select @change="setTreatment" :items="getAppointmentNames(appointmentTypes)" label="Behandlung"></v-select>
+                <v-text-field v-model="firstname" label="Vorname*" :rules="[rules.required]"></v-text-field>
+                <v-text-field v-model="lastname" label="Nachname*" :rules="[rules.required]"></v-text-field>
+                <v-text-field v-model="email" label="Email*" :rules="[rules.required, rules.email]"></v-text-field>
+                <v-text-field v-model="phonenumber" label="Telefon"></v-text-field>
+                <small>*indicates required field</small>
+              </v-card-text>
+            </v-flex>
+
+          <v-card-actions class="buttons">
+            <v-btn color="rgba(51, 169, 181, 255)" flat @click.stop="show=false">Abbrechen</v-btn>
+            <v-btn color="rgba(51, 169, 181, 255)" flat @click.stop="booking()">Buchen</v-btn>
+          </v-card-actions>
+          </v-layout>
+        </v-card>
+      </v-dialog>
+
+  </v-container>
+</template>
+
+<script>
+  export default {
+    props: [
+      'value',
+      'appointmentTypes',
+      'selectedDate'
+    ],
+    data() {
+      return {
+        rules: {
+          required: v => !!v || 'Required.',
+          email: v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+        },
+        treatmentselect: '',
+        firstname: '',
+        lastname: '',
+        email: '',
+        phonenumber: ''
+      }
+    },
+    methods: {
+      getAppointmentNames: appointmentTypes =>  {
+        return appointmentTypes.map(item => item.name)
+      },
+      booking: async function() {
+        const treatmentID = this.appointmentTypes.find(item => item.name === this.treatmentselect)
+
+        const res = await this.$axios.$post(`${process.env.ACUITYPROXY}/api/appointments`, {
+          data: {
+            appointmentTypeID: treatmentID, datetime: this.$dayjs(this.selectedDate), firstName: this.firstname, lastName: this.lastname, email: this.email
+          },
+          auth: {
+            username: process.env.ACUITYUSER, password: process.env.ACUITYPW
+          }})
+
+        console.log(res)
+      },
+      setTreatment: function(option) {
+        this.treatmentselect = option
+      },
+    },
+    computed: {
+      show: {
+        get () {
+          console.log('get is called')
+          return this.value
+        },
+        set (value) {
+          console.log('set is called', value)
+          this.$emit('input', value)
+        }
+      }
+    }
+  }
+</script>
+
+
+<style lang="scss" scoped>
+  .bookingDialog {
+    width: 50vw;
+    min-width: 300px;
+    height: 90vh;
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
---
+++ components/Calendar.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-container v-if="items.length > 0" fluid grid-list-md align-content-space-between>
+    <v-layout class="header">
+      <v-flex class="prev"><v-btn flat>prev</v-btn></v-flex>
+      <v-flex class="month">{{items[0].month}}</v-flex>
+      <v-flex class="next"><v-btn flat>next</v-btn></v-flex>
+    </v-layout>
+    <v-divider></v-divider>
+    <v-data-iterator
+            :items="items"
+            :pagination.sync="pagination"
+            content-tag="v-layout"
+            row
+            hide-actions
+    >
+      <template v-slot:item="props">
+        <v-flex xs12 sm6 md4 lg3>
+          <v-list dense>
+            <div class="dateContainer">
+              <div class="date">{{ props.item.date }}.</div>
+              <div class="weekday">{{ props.item.weekday }}</div>
+            </div>
+            <v-list-tile class="timecontainer" v-for="time in props.item.times" :key="props.item.date + time">
+              <v-list-tile-content class="timecontainer"><v-btn class="timebtn" flat @click="openBookingDialog(`${props.item.date} ${props.item.month} ${time}`)">{{time}}</v-btn></v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-flex>
+      </template>
+    </v-data-iterator>
+    <BookingForm  v-model="showBooking" :selectedDate="selectedDate" :appointmentTypes="appointmentTypes" ></BookingForm>
+  </v-container>
+  <h1 class="loading" v-else>Calendar is loading.....</h1>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  import BookingForm from '@/components/BookingForm'
+  export default {
+    watch: {
+      'dates' : function (val, oldVal) {
+        this.items = val
+      }
+    },
+    components: {
+      BookingForm
+    },
+    props: ['dates', 'appointmentTypes'],
+    data() {
+      return {
+        doctor: {},
+        pagination: {
+        rowsPerPage: 5
+        },
+         items: this.dates,
+        showBooking: false,
+        selectedDate: ''
+      }
+    },
+    methods: {
+      openBookingDialog(date) {
+        console.log(date)
+        this.selectedDate = date
+        this.showBooking = true
+      }
+    },
+    mounted() {
+      console.log('mounted calendar')
+    }
+
+  }
+</script>
+
+<style scoped lang="scss">
+  .loading {
+    text-align: center;
+    display: block;
+    height: 30vw;
+  }
+
+  .prev {
+    text-align: left;
+  }
+  .month {
+    text-align: center;
+    font-weight: bold;
+    font-size: 20pt;
+  }
+  .next {
+    text-align: right;
+  }
+
+  .timecontainer {
+    padding: 0 !important;
+  }
+
+  .timebtn {
+    align-self: center;
+  }
+
+  .dateContainer {
+    margin: 15px 0;
+    display: flex;
+    justify-content: center;
+    font-weight: bold;
+
+    .date {
+      font-size: 20pt;
+    }
+
+    .weekday {
+      font-size: 15pt;
+    }
+  }
+</style>
