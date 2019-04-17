a43ded75324c0e8e0dfe83e6e7462ce6725e0f69
Patrick Weingaertner <patrick.weingaertner91@gmail.com>
2019-04-17 10:19:06 +0200

    add vuex persist plugin and store file

---
+++ store/localStorage.js
@@ -0,0 +1,32 @@
+export const state = () => {
+  return {
+    doctors: []
+  }
+}
+
+export const mutations = {
+  SET_DOCTORS: (state, doctors) => {
+    state.doctors = doctors
+  },
+  ADD_DATES: (state, payload) => {
+    for (const doctor of state.doctors) {
+      if (doctor.iddoctorprofile === payload.id) {
+        doctor.allDates = payload.dates
+      }
+    }
+  },
+  ADD_APPOINTMENTTYPES: (state, payload) => {
+    for (const doctor of state.doctors) {
+      if (doctor.iddoctorprofile === payload.id) {
+        doctor.appointmentTypes = payload.appointmentTypes
+      }
+    }
+  }
+}
+
+export const getters = {
+  getAllDoctors: state => state.allDoctors,
+  getSpecificDoctor: (state) =>  (index) => {
+    return state.doctors.find(doctor => doctor.iddoctorprofile === index)
+  }
+}
