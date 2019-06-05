63b4c61dd0db12dd2b758deece98c057db4461df
Patrick Weingaertner <patrick.weingaertner91@gmail.com>
2019-06-05 14:26:13 +0200

    make footer higher and rearrange links

--- components/Yaofooter.vue
+++ components/Yaofooter.vue
@@ -1,32 +1,45 @@
 <template>
   <v-footer>
-    <v-layout justify-center row wrap>
-      <v-flex class="links">
-        <v-flex text--white>
-          <strong>Follow us</strong>
-          <a href="https://www.facebook.com/yaohealth"><fa class="socialicon" :icon="['fab', 'facebook-f']"/></a>
-          <a href="https://www.twitter.com/yaohealth"><fa class="socialicon" :icon="['fab', 'twitter']"/></a>
+    <v-container pa-0>
+      <v-layout justify-center column>
+        <v-flex class="links">
+          <v-layout justify-center column pt-3>
+            <v-flex text--white>
+              <strong>Follow us</strong>
+            </v-flex>
+            <v-flex pb-5>
+              <a href="https://www.twitter.com/yaohealth"><fa class="socialicon" :icon="['fab', 'twitter']"/></a>
+              <a href="https://www.instagram.com/yaohealth"><fa class="socialicon" :icon="['fab', 'instagram']"/></a>
+              <a href="https://www.facebook.com/yaohealth"><fa class="socialicon" :icon="['fab', 'facebook-f']"/></a>
+            </v-flex>
+            <v-flex>
+              <nuxt-link class="footerlink" to="/impressum">Impressum</nuxt-link>
+            </v-flex>
+            <v-flex>
+              <nuxt-link class="footerlink" to="/kontakt">Kontakt</nuxt-link>
+            </v-flex>
+            <v-flex pb-5>
+              <nuxt-link class="footerlink" to="/datenschutz">Datenschutz</nuxt-link>
+            </v-flex>
+          </v-layout>
         </v-flex>
-        <v-flex>
-          <nuxt-link class="footerlink" to="/impressum">Impressum</nuxt-link>
-          <nuxt-link class="footerlink" to="/kontakt">Kontakt</nuxt-link>
-          <nuxt-link class="footerlink" to="/datenschutz">Datenschutz</nuxt-link>
+        <v-flex class="grey darken-3" text-xs-center white--text xs12>
+          &copy;2019 — <strong>YAO</strong>
         </v-flex>
-      </v-flex>
-
-
-      <v-flex class="grey darken-3" text-xs-center white--text xs12>
-        &copy;2019 — <strong>YAO</strong>
-      </v-flex>
-    </v-layout>
+      </v-layout>
+    </v-container>
   </v-footer>
 </template>
 
 
 <style lang="scss" scoped>
+  $yaoGradient: radial-gradient(circle at center, #074f65 0, #00afa4 100%);
+
   .links {
-    background-color: rgba(51, 169, 181, 255);
+    background-image: $yaoGradient;
     padding: 10px;
+    display: flex;
+    flex-flow: column;
   }
 
   .socialicon {
