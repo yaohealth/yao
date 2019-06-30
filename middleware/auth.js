869cd5054203b245c5432c3dc31bf6f3aa25880f
Patrick Weingaertner <patrick.weingaertner91@gmail.com>
2019-06-30 12:03:50 +0200

    fix login bug

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,18 @@
 export default function ({store, redirect, route}) {
-  store.dispatch('auth/fetch').then(res => {
-    const userIsLoggedIn = !!res.user
-    const urlRequiresAuth = /^\/admin(\/|$)/.test(route.fullPath)
-    const urlRequiresNonAuth = /^\/login(\/|$)/.test(route.fullPath)
-    if (!userIsLoggedIn && urlRequiresAuth) {
+  const urlRequiresAuth = /^\/admin(\/|$)/.test(route.fullPath)
+  const urlRequiresNonAuth = /^\/login(\/|$)/.test(route.fullPath)
+  if (urlRequiresAuth) {
+    store.dispatch('auth/fetch').then(res => {
+      const userIsLoggedIn = !!res.user
+      if (!userIsLoggedIn && urlRequiresAuth) {
+        return redirect('/login')
+      }
+      if (userIsLoggedIn && urlRequiresNonAuth) {
+        return redirect('/admin')
+      }
+      return Promise.resolve()
+    }).catch(e => {
       return redirect('/login')
-    }
-    if (userIsLoggedIn && urlRequiresNonAuth) {
-      return redirect('/admin')
-    }
-    return Promise.resolve()
-  }).catch(e => {
-    return redirect('/login')
-  })
+    })
+  }
 }
